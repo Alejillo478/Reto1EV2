7a692fb22fc356d2b57ab1b0c524832160349665
bianca2912 <DAM1@aula11-pc26>
2025-01-20 13:43:01 +0100

    parte 2 hecha
    Merge branch 'main' of https://github.com/Alejillo478/Reto1EV2.git into
    main

--- 2EV RETO 1/src/Reto1/Funciones1.java
+++ 2EV RETO 1/src/Reto1/Funciones1.java
@@ -3,8 +3,10 @@ package Reto1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 import Clases.Empleado;
+import Clases.Producto;
 import Clases.Tienda;
 
 public class Funciones1 {
@@ -57,6 +59,58 @@ public static void mostrarTodasLasTiendas(List<Tienda> tiendas) {
     	}
 	}
 
+
+
+// ej 2 
+
+public class Compra {
+    
+    public static Tienda seleccionarTiendaPorId(List<Tienda> tiendas) {
+        Scanner scanner = new Scanner(System.in);
+        Tienda tiendaSeleccionada = null;
+        
+        while (tiendaSeleccionada == null) {
+            System.out.println("Listado de tiendas:");
+            for (Tienda tienda : tiendas) {
+                System.out.println("ID: " + tienda.getId() + " - Nombre: " + tienda.getNombre());
+            }
+            
+            System.out.print("Ingrese el ID de la tienda: ");
+            int idSeleccionado = scanner.nextInt();
+            
+            for (Tienda tienda : tiendas) {
+                if (tienda.getId() == idSeleccionado) {
+                    tiendaSeleccionada = tienda;
+                    break;
+                }
+            }
+            
+            if (tiendaSeleccionada == null) {
+                System.out.println("ID no válido. Inténtelo de nuevo.");
+            }
+        }
+        
+        return tiendaSeleccionada;
+    }
+
+    public static void comprarProducto(Tienda tienda, Cliente cliente) {
+        if (tienda.getListaProductos().isEmpty()) {
+            System.out.println("No hay productos disponibles en esta tienda.");
+            return;
+        }
+        
+        Random random = new Random();
+        Producto productoComprado = tienda.getListaProductos().remove(random.nextInt(tienda.getListaProductos().size()));
+        cliente.comprarProducto(tienda, productoComprado);
+        
+        System.out.println("Ha comprado: " + productoComprado.getNombre() + " por " + productoComprado.getPrecio() + " euros.");
+        
+        if (tienda.getListaProductos().size() < 5) {
+            tienda.reponerProductos(5);
+            System.out.println("Se han repuesto 5 productos en la tienda " + tienda.getNombre());
+        }
+    }
+}
 
 }
 	
